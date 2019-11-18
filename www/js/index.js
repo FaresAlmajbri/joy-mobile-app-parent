6ffc1a8f08736e3852cc4f92589c9fead62bc20f
fares-younis <fares.younis@elshaden.com>
2019-11-18 23:51:05 +0200

    feat: home and Select hHospital pages

--- www/js/index.js
+++ www/js/index.js
@@ -1,78 +1,79 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-ons.ready(function() {
-  var pullHook = document.getElementById('pull-hook');
+// /*
+//  * Licensed to the Apache Software Foundation (ASF) under one
+//  * or more contributor license agreements.  See the NOTICE file
+//  * distributed with this work for additional information
+//  * regarding copyright ownership.  The ASF licenses this file
+//  * to you under the Apache License, Version 2.0 (the
+//  * "License"); you may not use this file except in compliance
+//  * with the License.  You may obtain a copy of the License at
+//  *
+//  * http://www.apache.org/licenses/LICENSE-2.0
+//  *
+//  * Unless required by applicable law or agreed to in writing,
+//  * software distributed under the License is distributed on an
+//  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+//  * KIND, either express or implied.  See the License for the
+//  * specific language governing permissions and limitations
+//  * under the License.
+//  */
+// ons.ready(function() {
+//   var pullHook = document.getElementById('pull-hook');
+//
+//   pullHook.addEventListener('changestate', function(event) {
+//     var message = '';
+//
+//     switch (event.state) {
+//       case 'initial':
+//         message = 'Pull to refresh';
+//         break;
+//       case 'preaction':
+//         message = 'Release';
+//         break;
+//       case 'action':
+//         message = 'Loading...';
+//         break;
+//     }
+//
+//     pullHook.innerHTML = message;
+//   });
+//
+//   pullHook.onAction = function(done) {
+//     setTimeout(done, 1000);
+//   };
+// });
+//
+// window.fn={}
+// window.fn.exit= function () {
+//     navigator.app.exitApp();
+// }
+// let app = {
+//     // Application Constructor
+//     initialize: function() {
+//         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+//     },
 
-  pullHook.addEventListener('changestate', function(event) {
-    var message = '';
-
-    switch (event.state) {
-      case 'initial':
-        message = 'Pull to refresh';
-        break;
-      case 'preaction':
-        message = 'Release';
-        break;
-      case 'action':
-        message = 'Loading...';
-        break;
-    }
-
-    pullHook.innerHTML = message;
-  });
-
-  pullHook.onAction = function(done) {
-    setTimeout(done, 1000);
-  };
-});
-
-window.fn={}
-window.fn.exit= function () {
-    navigator.app.exitApp();
-}
-let app = {
-    // Application Constructor
-    initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-    },
 
     // deviceready Event Handler
     //
     // Bind any cordova events here. Common events are:
     // 'pause', 'resume', etc.
-    onDeviceReady: function() {
-        //  Screen Orientation lock code.
-        window.screen.orientation.lock('portrait');
-        //disable back button.
-        document.addEventListener("backbutton", function(e){
-            if($.mobile.activePage.is('#login_page')){
-                e.preventDefault();
-            }
-            else {
-                if (confirm("Are you sure you want to logout?")) {
-                    /* Here is where my AJAX code for logging off goes */
-                }
-                else {
-                    return false;
-                }
-            }
-        }, false);
+    // onDeviceReady: function() {
+    //     //  Screen Orientation lock code.
+    //     window.screen.orientation.lock('portrait');
+    //     //disable back button.
+    //     document.addEventListener("backbutton", function(e){
+    //         if($.mobile.activePage.is('#login_page')){
+    //             e.preventDefault();
+    //         }
+    //         else {
+    //             if (confirm("Are you sure you want to logout?")) {
+    //                 /* Here is where my AJAX code for logging off goes */
+    //             }
+    //             else {
+    //                 return false;
+    //             }
+    //         }
+    //     }, false);
 
         // document.getElementById("login").onclick = function() {alert('ees'); login()};
         // page.querySelector('#preReceiveButton').onclick = showTemplateDialog();
@@ -83,8 +84,8 @@ let app = {
         // };
 
 
-
-    },
+    //
+    // },
 
     // Update DOM on a Received Event
     // receivedEvent: function(id) {
@@ -158,23 +159,23 @@ let app = {
 //
 //         document.getElementById("showScanQr").addEventListener("click", QRScanner.show());
 //     }
-};
-
-function login(){
-    try{
-        let ifConnected = window.navigator.onLine;
-        if (ifConnected) {
-            // alert('Connection available');
-            // ons.notification.toast('Hi there!', { timeout: 1000, animation: 'fall' })
-        } else {
-            // alert('Connection not available');
-            ons.notification.toast('لا يوجد إتصال بالنترنت!', { timeout: 6000, animation: 'ascend' })
-
-        }
-    }catch (e) {
-        alert(e);
-        console.log(e);
-    }
-}
-
-app.initialize();
+// };
+//
+// function login(){
+//     try{
+//         let ifConnected = window.navigator.onLine;
+//         if (ifConnected) {
+//             // alert('Connection available');
+//             // ons.notification.toast('Hi there!', { timeout: 1000, animation: 'fall' })
+//         } else {
+//             // alert('Connection not available');
+//             ons.notification.toast('لا يوجد إتصال بالنترنت!', { timeout: 6000, animation: 'ascend' })
+//
+//         }
+//     }catch (e) {
+//         alert(e);
+//         console.log(e);
+//     }
+// }
+//
+// app.initialize();
